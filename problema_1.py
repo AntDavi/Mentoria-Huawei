34cad302c54fed494b02dd9935c619fd8d9ee3da
Uso Geral <user.geral@huaweilab.local>
2024-04-18 11:13:20 -0300

    push 1

---
+++ problema_1.py
@@ -0,0 +1,8 @@
+def soma_num():
+    num1 = int(input('Insira o primeiro numero: '))
+    num2 = int(input('Insira o segundo numero: '))
+
+    soma = num1 + num2;
+    print(f"A soma de {num1} e {num2} é:", soma)
+
+soma_num()
