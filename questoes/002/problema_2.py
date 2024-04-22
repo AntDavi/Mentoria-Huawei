e3b8b78890489310d6b455e914c01f14ad66cccd
AntDavi <ant.davi07@gmail.com>
2024-04-22 10:59:08 -0300

    atividade completa

--- questoes/002/problema_2.py
+++ questoes/002/problema_2.py
@@ -1,11 +1,8 @@
-# Solicita as notas bimestrais do usuário
 nota1 = float(input("Digite a nota do 1º bimestre: "))
 nota2 = float(input("Digite a nota do 2º bimestre: "))
 nota3 = float(input("Digite a nota do 3º bimestre: "))
 nota4 = float(input("Digite a nota do 4º bimestre: "))
 
-# Calcula a média aritmética e arredonda para uma casa decimal
 media = round((nota1 + nota2 + nota3 + nota4) / 4, 1)
 
-# Exibe a média
 print("A média aritmética do ano é: ", media)
---
+++ questoes/005/problema_5.py
@@ -0,0 +1,10 @@
+def verficar_maior():
+    num1 = int(input('Digite o primeiro número: '))
+    num2 = int(input('Digite o segundo número: '))
+
+    if num1 > num2:
+        print(f"{num1} é o maior")
+    else:
+        print(f"{num2} é o maior")
+
+verficar_maior()
